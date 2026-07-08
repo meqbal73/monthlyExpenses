62f57eaf2f55b8c168f7c3729c7e95ebb6436823
meqbal73 <51688022+meqbal73@users.noreply.github.com>
2026-07-08 13:13:42 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,12 @@
 // الصق إعدادات Web App من Firebase Console هنا.
 // لا تضع أي Service Account أو private key في مشروع GitHub.
-export const firebaseConfig = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDp6RHQkVZNPXXGPguuwNCzONYC4uAQee8",
+  authDomain: "monthlyexpenses-2e3f4.firebaseapp.com",
+  projectId: "monthlyexpenses-2e3f4",
+  storageBucket: "monthlyexpenses-2e3f4.firebasestorage.app",
+  messagingSenderId: "14644698781",
+  appId: "1:14644698781:web:5e20b193bd68de9d5dbe74",
+  measurementId: "G-2M497V55CC"
 };
